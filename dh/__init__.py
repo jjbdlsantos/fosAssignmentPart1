d32487abb2b8304e0d5d50a03460802817b7d96d
m-sachi <melisa.sachi@student.uts.edu.au>
2016-05-02 11:38:16 +1000

    Update __init__.py
    + Formatting
    + Debugging: in calculate_dh_secret, convert shared_secret to a string before converting it to bytes
    
    - Still need to change prime and hash

--- dh/__init__.py
+++ dh/__init__.py
@@ -18,31 +18,32 @@ C2007CB8 A163BF05 98DA4836 1C55D39A 69163FA8 FD24CF5F
 # Convert from the value supplied in the RFC to an integer
 prime = read_hex(raw_prime)
 
+
 # Project TODO: write the appropriate code to perform DH key exchange
 
 def create_dh_key():
     # Creates a Diffie-Hellman key
     # Returns (public, private)
-	
+
     # Selects a random private key (a) from Zp
-	private = random.randint(1, (prime - 1))
-	
-	# Calculates g^a mod p (2 ** private % prime)
-	public = pow(2, private, prime)
-	
-    return (public, private)
+    private = random.randint(1, (prime - 1))
+
+    # Calculates g^a mod p (2 ** private % prime)
+    public = pow(2, private, prime)
+
+    return public, private
+
 
 def calculate_dh_secret(their_public, my_private):
-    # Calculate the shared secret
+    # Calculate the shared secret & format as ascii bytes
     shared_secret = pow(their_public, my_private, prime)
-  
+    shared_secret = bytes(str(shared_secret), "ascii")
+
     # Hash the value so that:
     # (a) There's no bias in the bits of the output
     #     (there may be bias if the shared secret is used raw)
     # (b) We can convert to raw bytes easily
     # (c) We could add additional information if we wanted
     # Feel free to change SHA256 to a different value if more appropriate
-    shared_hash = SHA256.new(bytes(shared_secret, "ascii")).hexdigest()
+    shared_hash = SHA256.new(shared_secret).hexdigest()
     return shared_hash
-
-	
