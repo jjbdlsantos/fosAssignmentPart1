a0a9d9b7749dfeafe2b427bb7d693621b0cd1819
m-sachi <melisa.sachi@student.uts.edu.au>
2016-05-13 22:39:57 +1000

    Update comms.py
    + Debugged replay attack
    + Incorporated HMACs (integrity)
    - Need to comment and move sections into functions
    - Need to add AES

--- lib/comms.py
+++ lib/comms.py
@@ -3,8 +3,10 @@ import struct
 from Crypto.Cipher import XOR
 
 from dh import create_dh_key, calculate_dh_secret
-from Crypto import Random
+
 from Crypto.Random.Fortuna import FortunaGenerator
+from Crypto.Hash import HMAC
+from Crypto.Hash import SHA256
 
 class StealthConn(object):
     def __init__(self, conn, client=False, server=False, verbose=False):
@@ -13,14 +15,19 @@ class StealthConn(object):
         self.client = client
         self.server = server
         self.verbose = verbose
+
+        self.shared_hash = None
+        self.cipher_key = None
+        self.hmac_key = None
+        self.is_initialised = False
         self.sendprng = None
         self.recvprng = None
+
         self.initiate_session()
 
     def initiate_session(self):
         # Perform the initial connection handshake for agreeing on a shared secret
 
-        ### TODO: Your code here!
         # This can be broken into code run just on the server or just on the client
         if self.server or self.client:
             my_public_key, my_private_key = create_dh_key()
@@ -29,32 +36,50 @@ class StealthConn(object):
             # Receive their public key
             their_public_key = int(self.recv())
             # Obtain our shared secret
-            shared_hash = calculate_dh_secret(their_public_key, my_private_key)
-            print("Shared hash: {}".format(shared_hash))
+            self.shared_hash = calculate_dh_secret(their_public_key, my_private_key)
+            print("Shared hash: {}".format(self.shared_hash))
+
+            # TODO: PRNG using shared_secret as the seed for self.cipher_key and self.hmac_key
+            # Use the shared secret as the seed for a PRNG to generate keys
+            prng = FortunaGenerator.AESGenerator()
+            prng.reseed(bytes(self.shared_hash, "ascii"))
+            # Randomly generate a 128-bit key for hmac
+            self.hmac_key = prng.pseudo_random_data(128)
 
         # Default XOR algorithm can only take a key of length 32
-        self.cipher = XOR.new(shared_hash[:4])
+        self.cipher = XOR.new(self.shared_hash[:4])
+        self.is_initialised = True
 
     def send(self, data):
-        
-        if self.cipher:
+        if self.is_initialised:
             if self.sendprng == None: #if we don't have a PRNG then make one
-                DHString = str(shared_secret) #making it out of this, which becomes the seed
-                A, B = DHString[:len(DHString)/2], DHString[len(DHString)/2:]
-                A = A.encode("ascii")
-                B = B.encode("ascii")
-                
-                self.sendprng = FortunaGenerator.AESGenerator() #making the "A" PRNG
-                self.sendprng.reseed(A)
-                self.recvprng = FortunaGenerator.AESGenerator() #making the "B" PRNG
-                self.recvprng.reseed(B)
+                # Making it out of the DH secret, which becomes the seed
+                index = int(len(self.shared_hash) / 2)
+                seed_a = self.shared_hash[:index]
+                seed_b = self.shared_hash[index:]
+                seed_a = seed_a.encode("ascii")
+                seed_b = seed_b.encode("ascii")
 
-            packet_id = self.sendprng.pseudo_random_data(128) #starting or continuing the sequence (generating the next number in the sequence)
-            data = data + packet_id
-            
-            encrypted_data = self.cipher.encrypt(data)
+                self.sendprng = FortunaGenerator.AESGenerator() #making the "A" PRNG
+                self.sendprng.reseed(seed_a)
+                self.recvprng = FortunaGenerator.AESGenerator() #making the "B" PRNG
+                self.recvprng.reseed(seed_b)
+
+            message_id = self.sendprng.pseudo_random_data(128) #starting or continuing the sequence (generating the next number in the sequence)
+
+            hmac = HMAC.new(self.hmac_key, digestmod=SHA256)  # TODO: is SHA256 a good idea?
+            hmac.update(data)
+
+            print("First ID: {}".format(message_id))
+
+            message = data + message_id + bytes(hmac.hexdigest(), "ascii")
+
+            encrypted_data = self.cipher.encrypt(message)
             if self.verbose:
                 print("Original data: {}".format(data))
+                print("Message ID: {}".format(message_id))
+                print("HMAC: {}".format(hmac.hexdigest()))
+                print("Data + Message ID + HMAC: {}".format(message))
                 print("Encrypted data: {}".format(repr(encrypted_data)))
                 print("Sending packet of length {}".format(len(encrypted_data)))
         else:
@@ -66,43 +91,61 @@ class StealthConn(object):
         self.conn.sendall(encrypted_data)
 
     def recv(self):
-            
         # Decode the data's length from an unsigned two byte int ('H')
         pkt_len_packed = self.conn.recv(struct.calcsize('H'))
         unpacked_contents = struct.unpack('H', pkt_len_packed)
         pkt_len = unpacked_contents[0]
 
         encrypted_data = self.conn.recv(pkt_len)
-        if self.cipher: #it's in here because it's after the channel is open
+        if self.is_initialised: #it's in here because it's after the channel is open
             if self.sendprng == None: #if we don't have a PRNG then make one
-                DHString = str(shared_secret) #making it out of this, which becomes the seed
-                A, B = DHString[:len(DHString)/2], DHString[len(DHString)/2:]
-                A = A.encode("ascii")
-                B = B.encode("ascii")
+                index = int(len(self.shared_hash) / 2)
+                seed_a = self.shared_hash[:index]
+                seed_b = self.shared_hash[index:]
+                seed_a = seed_a.encode("ascii")
+                seed_b = seed_b.encode("ascii")
                 
                 self.sendprng = FortunaGenerator.AESGenerator() #making the "B" PRNG
-                self.sendprng.reseed(B)
+                self.sendprng.reseed(seed_b)
                 self.recvprng = FortunaGenerator.AESGenerator() #making the "A" PRNG
-                self.recvprng.reseed(A)
+                self.recvprng.reseed(seed_a)
 
-            packet_id = self.sendprng.pseudo_random_data(128) #starting or continuing the sequence (generating the next number in the sequence)
+            message = self.cipher.decrypt(encrypted_data)
+            data = message[:-192] #gets me the message
+            received_id = message[-192:-64] #gets me the packet id
+            hmac_recv = message[-64:]
 
-            data = self.cipher.decrypt(encrypted_data)
-            data = data[:-128] #gets me the message
-            received_id = data[-128:] #gets me the packet id
+            # Generate the expected message_id
+            message_id = self.recvprng.pseudo_random_data(128) #starting or continuing the sequence (generating the next number in the sequence)
 
-            if received_id == packet_id: #if the id I received matches the id that was sent (is expected) then great!
-                print("Legit")
+            print("Expected ID: {}".format(message_id))
 
-            else: #otherwise it's from a bad person so delete it!
-                print("Not legit, ABORT")
-                data = None #get rid of the message because it's a fake!
-            
-            
+            # Generate the correct HMAC
+            hmac = HMAC.new(self.hmac_key, digestmod=SHA256)
+            hmac.update(data)
+
             if self.verbose:
                 print("Receiving packet of length {}".format(pkt_len))
                 print("Encrypted data: {}".format(repr(encrypted_data)))
                 print("Original data: {}".format(data))
+                print("ID Received: {}".format(received_id))
+                print("ID Expected: {}".format(message_id))
+                print("HMAC Received: {}".format(hmac_recv))
+                print("HMAC Expected: {}".format(hmac))
+
+            if received_id == message_id: #if the id I received matches the id that was sent (is expected) then great!
+                print("Message IDs match!")
+                # Check if the generated and received HMACs match
+                if bytes(hmac.hexdigest(), "ascii") == hmac_recv:
+                    # If they do, assume the data received has not been tampered with
+                    print("HMACs match!")
+                else:
+                    # If the HMACs don't match, discard the data and return None
+                    print("HMACs don't match!")
+                    data = None
+            else: #otherwise it's from a bad person so delete it!
+                print("Not legit, ABORT")
+                data = None #get rid of the message because it's a fake!
         else:
             data = encrypted_data
 
@@ -110,3 +153,4 @@ class StealthConn(object):
 
     def close(self):
         self.conn.close()
+
